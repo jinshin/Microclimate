6eda803cfd021ae533edf62a1955ec89e3b1b34b
Sergei Lyubshin <n0p@ukr.net>
2018-06-06 11:10:39 +0300

    Changes for OrangePi, cleanup.

--- co2.py
+++ co2.py
@@ -1,6 +1,6 @@
 import serial, sched, time, socket
 
-VentoIP = '10.10.10.10'
+VentoIP = '192.168.0.20'
 VentoPort = 4000
 
 #Min
@@ -12,83 +12,83 @@ Temp = 0
 Power_On = 0
 
 def get_co2():
-        global Temp
-        ser = serial.Serial('/dev/ttyAMA0', baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=0.5)
-        try:
-                result=ser.write(bytes([0xff,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79]))
-        except:
-                return 0
-        time.sleep(0.1)
-        try:
-                s=ser.read(9)
-        except:
-                return 0
-        chk = (s[1]+s[2]+s[3]+s[4]+s[5]+s[6]+s[7])%256
-        chk = (0xff - chk) + 1
-        if chk != s[8]:
-                #CRC invalid, return 0
-                return 0
-        if s[0] == 0xff and s[1] == 0x86:
-                Temp = s[4]-40
-                return s[2]*256 + s[3]
+    global Temp
+#use /dev/ttyAMA0 in Raspberry Pi
+    ser = serial.Serial('/dev/ttyS2', baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=0.5)
+    try:
+        result=ser.write(bytes([0xff,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79]))
+    except:
+        return 0
+    time.sleep(0.1)
+    try:
+        s=ser.read(9)
+    except:
+        return 0
+    chk = (s[1]+s[2]+s[3]+s[4]+s[5]+s[6]+s[7])%256
+    chk = (0xff - chk) + 1
+    if chk != s[8]:
+        #CRC invalid, return 0
+        return 0
+    if s[0] == 0xff and s[1] == 0x86:
+        Temp = s[4]-40
+        return s[2]*256 + s[3]
 
 def set_speed(speed):
-        global VentoIP
-        global VentoPort
-        Device = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        Device.settimeout(1)
-        sendstr = bytes('mobile','cp1251')
-        sendstr = sendstr + bytes([0x05]) + (Vent_Speed.to_bytes(1, byteorder='big')) + bytes([0xD, 0xA])
-        #print (sendstr)
-        Device.sendto(sendstr, (VentoIP, VentoPort))
-        Device.close()
-
+    global VentoIP
+    global VentoPort
+    Device = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    Device.settimeout(1)
+    sendstr = bytes('mobile','cp1251')
+    sendstr = sendstr + bytes([0x05]) + (speed.to_bytes(1, byteorder='big')) + bytes([0xD, 0xA])
+    Device.sendto(sendstr, (VentoIP, VentoPort))
+    Device.close()
+        
 def switch_power():
-        global VentoIP
-        global VentoPort
-        Device = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        Device.settimeout(1)
-        sendstr = bytes('mobile','cp1251')
-        sendstr = sendstr + bytes([0x03, 0x01, 0xD, 0xA])
-        Device.sendto(sendstr, (VentoIP, VentoPort))
-        Device.close()
+    global VentoIP
+    global VentoPort
+    Device = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    Device.settimeout(1)
+    sendstr = bytes('mobile','cp1251')
+    sendstr = sendstr + bytes([0x03, 0x01, 0xD, 0xA])
+    Device.sendto(sendstr, (VentoIP, VentoPort))
+    Device.close()
 
 def get_speed():
-        global VentoIP
-        global VentoPort
-        global Power_On
-        Device = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        Device.settimeout(1)
-        sendstr = bytes('mobile','cp1251')
-        sendstr = sendstr + bytes([0x01, 0xD, 0xA])
-        try:
-                Device.sendto(sendstr, (VentoIP, VentoPort))
-        except:
-                return 0
-        try:
-                Received, addr = Device.recvfrom(256)
-        except:
-                return 0
-        Device.close()
-        if Received[0:6] == b'master':
-                X = 6
-                while X < len (Received):
-                        SW = Received[X]
-                        if SW == 3:
-                                X+=1
-                                Power_On = Received[X]
-                                #print('Power: ',Received[X])
-                        if SW == 4:
-                                X+=1
-                                #print('Preset speed: ',Received[X])
-                                if Received[X] != 4:
-                                        return (22+(Received[X]-1)*((255-22)/2))
-                        if SW == 5:
-                                X+=1
-                                #print('Manual speed: ',Received[X])
-                                return (Received[X])
-                        X+=1
+    global VentoIP
+    global VentoPort
+    global Power_On
+    Device = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    Device.settimeout(1)
+    sendstr = bytes('mobile','cp1251')
+    sendstr = sendstr + bytes([0x01, 0xD, 0xA])
+    try:
+        Device.sendto(sendstr, (VentoIP, VentoPort))
+    except:
         return 0
+    try:
+        Received, addr = Device.recvfrom(256)
+    except:
+        return 0
+    Device.close()
+    if Received[0:6] == b'master':
+        X = 6
+        while X < len (Received):
+            SW = Received[X]
+            if SW == 3:
+                X+=1
+                Power_On = Received[X]
+                #print('Power: ',Received[X])
+            elif SW == 4:
+                X+=1
+                #print('Preset speed: ',Received[X])
+                if Received[X] != 4:
+                    return (22+(Received[X]-1)*((255-22)/2))
+            elif SW == 5:
+                X+=1
+                #print('Manual speed: ',Received[X])
+                return (Received[X])
+            X+=1
+    return 0
 
 CO2_Prev = 0
 Speed_Prev = 0
@@ -103,7 +103,7 @@ def change_speed(speed_val):
      elif Vent_Speed < 22:
         Vent_Speed = 22
      if Vent_Speed != Speed_Prev:
-         set_speed(Vent_Speed)
+        set_speed(Vent_Speed)
 
 def time_func():
     global CO2_Level
@@ -127,7 +127,7 @@ def time_func():
     if CO2_Level > 800 and Power_On == 0:
         switch_power()
     CO2_Prev = CO2_Level
-    log_file = open("/var/log/co2.log", "a")
+    log_file = open("/var/log/co2", "a")
     log_file.write(time.strftime("%Y-%m-%d %H:%M")+" "+str(CO2_Level)+" "+str(Temp)+" "+str(Vent_Speed)+" "+str (Power_On)+"\r\n")
     log_file.close()
 
@@ -141,4 +141,4 @@ s.enter(1, 1, time_func, ())
 s.run()
 
 while True:
-        time.sleep(1)
+    time.sleep(1)
