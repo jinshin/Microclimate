ab042a59d740b75061a0c27d58df3a68bdc3b069
Sergei Lyubshin <n0p@ukr.net>
2020-01-13 21:07:50 +0200

    Implemented day/time speed limits for noise comfort. Also, "Cloud control option" in Blauberg App now enables/disables control by script (useful for filter maintenance)

--- co2.py
+++ co2.py
@@ -1,4 +1,4 @@
-import serial, sched, time, socket
+import serial, sched, time, socket, datetime
 
 VentoIP = '192.168.0.20'
 VentoPort = 4000
@@ -10,6 +10,23 @@ CO2_Prev = CO2_Level
 Speed_Prev = Vent_Speed
 Temp = 0
 Power_On = 0
+Do_Control = 0
+
+Night_Limit = 120
+Day_Limit = 250
+
+Night_Start = datetime.time(23,0,0)
+Day_Start = datetime.time(9,0,0)
+
+def is_night():
+    global Night_Start
+    global Day_Start
+    Current_Time = datetime.datetime.now().time()
+    if Night_Start <= Day_Start:
+        return Night_Start <= Current_Time <= Day_Start
+    else:
+        return Night_Start <= Current_Time or Current_Time <= Day_Start
+
 
 def get_co2():
     global Temp
@@ -42,7 +59,7 @@ def set_speed(speed):
     sendstr = sendstr + bytes([0x05]) + (speed.to_bytes(1, byteorder='big')) + bytes([0xD, 0xA])
     Device.sendto(sendstr, (VentoIP, VentoPort))
     Device.close()
-        
+
 def switch_power():
     global VentoIP
     global VentoPort
@@ -90,26 +107,66 @@ def get_speed():
             X+=1
     return 0
 
+def get_settings():
+    global VentoIP
+    global VentoPort
+    global Do_Control
+    Device = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    Device.settimeout(1)
+    sendstr = bytes('mobile','cp1251')
+    sendstr = sendstr + bytes([0x02, 0xD, 0xA])
+    try:
+        Device.sendto(sendstr, (VentoIP, VentoPort))
+    except:
+        return 0
+    try:
+        Received, addr = Device.recvfrom(256)
+    except:
+        return 0
+    Device.close()
+    if Received[0:6] == b'master':
+        X = 6
+        while X < len (Received):
+            SW = Received[X]
+            if SW == 31:
+                X+=1
+                Do_Control = Received[X]
+            X+=1
+    return 0
+
+
 CO2_Prev = 0
 Speed_Prev = 0
 
 def change_speed(speed_val):
-     global Speed_Prev
-     global Vent_Speed
-     Speed_Prev = Vent_Speed
-     Vent_Speed = Vent_Speed + speed_val
-     if Vent_Speed > 255:
-        Vent_Speed = 255
-     elif Vent_Speed < 22:
-        Vent_Speed = 22
-     if Vent_Speed != Speed_Prev:
-        set_speed(Vent_Speed)
+    global Speed_Prev
+    global Vent_Speed
+    global Night_Limit
+    global Day_Limit
+
+    Speed_Limit = 255
+    if (is_night()):
+       Speed_Limit = Night_Limit
+    else:
+       Speed_Limit = Day_Limit
+
+    Speed_Prev = Vent_Speed
+    Vent_Speed = Vent_Speed + speed_val
+    #limit max speed in night
+    if Vent_Speed > Speed_Limit:
+       Vent_Speed = Speed_Limit
+    elif Vent_Speed < 22:
+       Vent_Speed = 22
+    if Vent_Speed != Speed_Prev:
+       set_speed(Vent_Speed)
 
 def time_func():
     global CO2_Level
     global Vent_Speed
     global CO2_Prev
     global Speed_Prev
+    global Night_LImit
+    global Day_Limit
     s.enter(60, 1, time_func, ())
     CO2_Level = get_co2()
     if CO2_Level == 0:
@@ -117,24 +174,25 @@ def time_func():
     Vent_Speed = get_speed()
     if Vent_Speed == 0:
         return
-    #print (CO2_Level, Temp, Vent_Speed)
-    if CO2_Level > 1000 and CO2_Prev < CO2_Level:
+    get_settings()
+    #print (CO2_Level, Temp, Vent_Speed, Do_Control)
+    if Do_Control == 1:
+      if CO2_Level > 1000 and CO2_Prev < CO2_Level:
         change_speed(12)
-    elif CO2_Level < 750 and CO2_Prev >= CO2_Level:
+      elif CO2_Level < 750 and CO2_Prev >= CO2_Level:
         change_speed(-12)
-    if CO2_Level <= 650 and Vent_Speed == 22 and Power_On == 1:
+      if CO2_Level <= 650 and Vent_Speed == 22 and Power_On == 1:
         switch_power()
-    if CO2_Level > 800 and Power_On == 0:
+      if CO2_Level > 800 and Power_On == 0:
         switch_power()
     CO2_Prev = CO2_Level
     log_file = open("/var/log/co2", "a")
     log_file.write(time.strftime("%Y-%m-%d %H:%M")+" "+str(CO2_Level)+" "+str(Temp)+" "+str(Vent_Speed)+" "+str (Power_On)+"\r\n")
     log_file.close()
 
-print("Starting.")
-
 CO2_Prev = get_co2()
 Speed_Prev = get_speed()
+print("Starting:",CO2_Prev,Speed_Prev)
 
 s = sched.scheduler(time.time, time.sleep)
 s.enter(1, 1, time_func, ())
